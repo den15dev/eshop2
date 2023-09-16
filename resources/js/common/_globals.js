7552e154c980b24b4c8b76ef8b7469f3e05739e2
den15 <dendangler@gmail.com>
2023-09-16 04:04:37 +0300

    v2.0.1.14 jQuery removed - test opacity 1

--- resources/js/common/_globals.js
+++ resources/js/common/_globals.js
@@ -1,5 +1,5 @@
 export const pageTint = document.querySelector('.main-tint');
-export const fadeSpeed = 100;
+export const fadeSpeed = 500;
 export const htmlElem = document.querySelector('html');
 export const lgMedia = window.matchMedia('(min-width: 992px)');
 export const smMedia = window.matchMedia('(min-width: 576px)');
