5e9fed2a1cb4766bbf3cfce7e6d6b1d9e9b87a88
den15dev <dendangler@gmail.com>
2024-08-07 18:32:48 +0300

    v2.0.4.7 Fix for fake users can make orders

--- app/Modules/Orders/Requests/OrderRequest.php
+++ app/Modules/Orders/Requests/OrderRequest.php
@@ -23,10 +23,14 @@ class OrderRequest extends FormRequest
     {
         $phone_num = '/^\+?[0-9]{0,3}[\s-]{0,2}\(?[0-9]{3}\)?[\s-]{0,2}[0-9]{3}[\s-]?[0-9]{2}[\s-]?[0-9]{2}$/';
 
+        // For fake users to make orders
+        $email_rules = ['nullable', 'max:255'];
+        if (!preg_match('/@tmail.tst$/', $this->email)) $email_rules[] = 'email:rfc,dns';
+
         return [
             'name' => ['required', 'min:2', 'max:255'],
             'phone' => ['required', 'max:25', 'regex:' . $phone_num],
-            'email' => ['nullable', 'email:rfc,dns', 'max:255'],
+            'email' => $email_rules,
             'delivery_method' => ['required', 'in:delivery,self-delivery'],
             'payment_method' => [Rule::enum(PaymentMethod::class)],
             'delivery_address' => ['exclude_if:delivery_method,self-delivery', 'required', 'min:7'],
